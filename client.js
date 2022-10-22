b4877e9e9f375cc8d2ccd84e29cd79a0190227c3
Dipam Sen <dipamdiptam@gmail.com>
2022-10-23 01:42:57 +0530

    create poc

--- client.js
+++ client.js
@@ -1,7 +1,64 @@
-class Client {
-  cookie;
+import axios from "axios";
+import fs from "fs";
+
+/** @type {import("axios").Axios} */
+const p5 = axios.create({
+  baseURL: "https://editor.p5js.org/",
+});
+
+export default class Client {
   constructor() {
     this.cookie = "";
+    this.config = {};
+    this.userInfo = {};
   }
-  login({ username, password }) {}
+  async login({ username, email, password }) {
+    this.config.credentials = { email: email || username, password };
+    try {
+      const res = await p5.post("/editor/login", {
+        email: email || username,
+        password,
+      });
+      this.userInfo = res.data;
+      this.cookie = res.headers["set-cookie"][0];
+    } catch (err) {
+      console.error(err.response.data.message);
+    }
+  }
+  async getSketch(id) {
+    const res = await p5.get(
+      `/editor/${this.userInfo.username}/projects/${id}`,
+      {
+        headers: { Cookie: this.cookie },
+      }
+    );
+    return res.data;
+  }
+  async updateSketch(sketch, file, content) {
+    const currTime = new Date().toISOString();
+    const res = await p5.put(
+      `/editor/projects/${sketch.id}`,
+      {
+        files: sketch.files.map((f) => {
+          if (f.name === file) {
+            f.content = content;
+            f.updatedAt = currTime;
+          }
+          return f;
+        }),
+        id: sketch.id,
+        isSaving: false,
+        owner: sketch.user,
+        updatedAt: currTime,
+      },
+      {
+        headers: { Cookie: this.cookie },
+      }
+    );
+    if (res.data.error) {
+      console.error(res.data.error);
+    } else {
+      return "Successfully updated sketch";
+    }
+  }
 }
---
+++ index.js
@@ -0,0 +1,85 @@
+import Client from "./client.js";
+import dotenv from "dotenv";
+import inquirer from "inquirer";
+
+dotenv.config();
+
+async function main() {
+  const client = new Client();
+
+  if (process.env.P5_USERNAME && process.env.P5_PASSWORD) {
+    await client.login({
+      username: process.env.P5_USERNAME,
+      password: process.env.P5_PASSWORD,
+    });
+  } else {
+    const { username, password } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "username",
+        message: "Enter your p5.js username",
+      },
+      {
+        type: "password",
+        name: "password",
+        message: "Enter your p5.js password",
+      },
+    ]);
+    await client.login({ username, password });
+  }
+
+  const allSketches = await client.getSketch("");
+
+  const { sketchId } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "sketchId",
+      message: "Choose Sketch",
+      choices: allSketches.map((s) => ({
+        name: `${s.name} (${s.id})`,
+        value: s.id,
+        short: s.id,
+      })),
+      loop: false,
+    },
+  ]);
+  const sketch = await client.getSketch(sketchId);
+  const fileNames = sketch.files
+    .filter((f) => f.fileType === "file" && f.content)
+    .map((f) => f.name);
+  const { file } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "file",
+      message: "Which file do you want to edit?",
+      choices: fileNames,
+    },
+  ]);
+  const fileContent = sketch.files.find((f) => f.name === file).content;
+  console.log(fileContent);
+
+  const { newContent } = await inquirer.prompt([
+    {
+      type: "editor",
+      name: "newContent",
+      message: "Edit the file",
+      postfix: "." + file.split(".").pop(),
+      default: fileContent,
+    },
+  ]);
+  console.log(newContent);
+
+  const { save } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "save",
+      message: "Do you want to update your changes to the web editor?",
+    },
+  ]);
+  if (save) {
+    const res = await client.updateSketch(sketch, file, newContent);
+    console.log(res);
+  }
+}
+
+main();
